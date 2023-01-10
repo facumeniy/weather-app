24d93b6b3b0ade53e7006e2dd2618564618102cf
Facundo Meniy <facundomeniy@gmail.com>
2023-01-10 15:55:44 -0300

    wip

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // SELECTORS
 const searchBar = document.getElementById('search-bar');
 const searchBtn = document.getElementById('search-btn');
+const celsiusBtn = document.getElementById('celsius');
+const fahrBtn = document.getElementById('fahrenheit');
 
 const cityInfo = document.getElementById('city-info');
 const cityName = document.getElementById('city-name');
@@ -9,13 +11,28 @@ const cityDay = document.getElementById('city-day');
 const cityHour = document.getElementById('city-hour');
 const tempSelect = document.getElementById('change-temp');
 const weatherIcon = document.getElementById('weather-icon');
+const feelksLike = document.getElementById('feels-like');
+const cityHumidity = document.getElementById('city-humidity');
+const windSpeed = document.getElementById('wind-speed');
 
+const firstDay = document.getElementById('first-day');
+const secondDay = document.getElementById('second-day');
+const thirdDay = document.getElementById('third-day');
+const fourthDay = document.getElementById('fourth-day');
+const fifthDay = document.getElementById('fifth-day');
+
+const firstTemp = document.getElementById('first-temp');
+const secondTemp = document.getElementById('second-temp');
+const thirdTemp = document.getElementById('third-temp');
+const fourthTemp = document.getElementById('fourth-temp');
+const fifthTemp = document.getElementById('fifth-temp');
+
+
+let day = "";
 // FUNCTIONS
 async function getCityInfo(city){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=d6c5ea0d9f6334f0d51cb2e9112e333b`, {mode: 'cors'})
     const cityData = await response.json();
-    
-    console.log(cityData);
 
     return{
         name: cityData.name,
@@ -33,6 +50,33 @@ async function getCityInfo(city){
         windSpeedMph: `${Math.round((cityData.wind.speed) * 2.236936)} mph`,
         windSpeedKmh: `${Math.round((cityData.wind.speed) * 3.6)} km/h`,
         timezone: cityData.timezone,
+    }
+}
+
+async function getCityForecast(city){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=d6c5ea0d9f6334f0d51cb2e9112e333b`, {mode: 'cors'})
+    const cityData = await response.json();
+
+    return {
+        oneCel: `${Math.round(cityData.list[6].main.temp_min - 273.15)} °C`,
+        oneFahr: `${((cityData.list[6].main.temp_min - 273.15) * 9/5 + 32).toFixed(0)} °F`,
+        oneWeather: cityData.list[6].weather[0].description,
+
+        twoCel: `${Math.round(cityData.list[14].main.temp_min - 273.15)} °C`,
+        twoFahr: `${((cityData.list[14].main.temp_min - 273.15) * 9/5 + 32).toFixed(0)} °F`,
+        twoWeather: cityData.list[14].weather[0].description,
+
+        threeCel: `${Math.round(cityData.list[22].main.temp_min - 273.15)} °C`,
+        threeFahr: `${((cityData.list[22].main.temp_min - 273.15) * 9/5 + 32).toFixed(0)} °F`,
+        threeWeather: cityData.list[22].weather[0].description,
+
+        fourCel: `${Math.round(cityData.list[30].main.temp_min - 273.15)} °C`,
+        fourFahr: `${((cityData.list[30].main.temp_min - 273.15) * 9/5 + 32).toFixed(0)} °F`,
+        fourWeather: cityData.list[30].weather[0].description,
+        
+        fiveCel: `${Math.round(cityData.list[38].main.temp_min - 273.15)} °C`,
+        fiveFahr: `${((cityData.list[38].main.temp_min - 273.15) * 9/5 + 32).toFixed(0)} °F`,
+        fiveWeather: cityData.list[38].weather[0].description,
     }
 }
 
@@ -63,7 +107,8 @@ function updateCity(city){
         const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         const date = new Date();
-        let day = parseInt(date.getDay());
+        let dayOfMonth = parseInt(date.getDate());
+        day = parseInt(date.getDay());
         const month = date.getMonth();
         const year = date.getFullYear();
         // HOUR IN COUNTRY
@@ -72,27 +117,93 @@ function updateCity(city){
         let currentHour = ((currentTime.getHours() + timezoneOffset / 60 / 60) + 3);
         if(currentHour > 24){
             currentHour = `0${currentHour - 24}`;
+            dayOfMonth++;
             day++;
+
+            const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+            let nextDay = (new Date().getDay() + 1);
+            const week = [];
+
+            for (let i = 0; i < 6; i++) {
+                const dayIndex = (nextDay + i) % 7;
+                week.push((days[dayIndex]));
+            }
+
+            firstDay.innerHTML = week[0];
+            secondDay.innerHTML = week[1];
+            thirdDay.innerHTML = week[2];
+            fourthDay.innerHTML = week[3];
+            fifthDay.innerHTML = week[4];
         }
         let currentMinute = currentTime.getMinutes();
         if(currentMinute < 10){
             currentMinute = `0${currentMinute}`;
         }
-        cityDay.innerText = `${days[day]}, ${months[month]} ${day} of ${year}`;
+        cityDay.innerText = `${days[day]}, ${months[month]} ${dayOfMonth} of ${year}`;
         cityHour.innerText = `${currentHour}:${currentMinute}`;
+
+        feelksLike.innerText = city.feelsLikeCel;
+        cityHumidity.innerText = city.humidity;
+        windSpeed.innerText = city.windSpeedKmh;
+    })
+}
+
+function updateForecast(city){
+    city.then(city => {
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let nextDay = (new Date().getDay() + 1);
+        const week = [];
+
+        for (let i = 0; i < 6; i++) {
+            const dayIndex = (nextDay + i) % 7;
+            week.push((days[dayIndex]));
+        }
+
+        firstDay.innerHTML = week[0];
+        secondDay.innerHTML = week[1];
+        thirdDay.innerHTML = week[2];
+        fourthDay.innerHTML = week[3];
+        fifthDay.innerHTML = week[4];
+
+        firstTemp.innerHTML = city.oneCel;
+        secondTemp.innerHTML = city.twoCel;
+        thirdTemp.innerHTML = city.threeCel;
+        fourthTemp.innerHTML = city.fourCel;
+        fifthTemp.innerHTML = city.fiveCel;
     })
 }
 
 // ONLOAD
-let city = getCityInfo("buenos aires");
-updateCity(city);
+let currentCity = getCityInfo("buenos aires");
+let cityForecast = getCityForecast("buenos aires");
+
+updateCity(currentCity);
+updateForecast(cityForecast);
+
 tempSelect.style.display = "block";
-  
 // EVENT LISTENERS
 searchBtn.addEventListener('click', () => {
-    let city = getCityInfo(searchBar.value);
+    currentCity = getCityInfo(searchBar.value);
+    const newForecast = getCityForecast(searchBar.value);
 
-    updateCity(city);
+    updateCity(currentCity);
+    updateForecast(newForecast);
+});
+
+celsiusBtn.addEventListener('click', () => {
+    currentCity.then(city => {
+        cityTemp.innerText = city.celsiusTemp;
+        feelksLike.innerText = city.feelsLikeCel;
+        windSpeed.innerText = city.windSpeedKmh;
+    })
+})
+
+fahrBtn.addEventListener('click', () => {
+    currentCity.then(city => {
+        cityTemp.innerText = city.fahrenheitTemp;
+        feelksLike.innerText = city.feelsLikeFahr;
+        windSpeed.innerText = city.windSpeedMph;
+    })
 })
 
 
